c119fa504ce3edd991d99899754068d7ce300560
radomirKrastev <radomirkostantinov@gmail.com>
2019-08-11 15:52:11 +0300

    created ArrayList

---
+++ Linear-Data-Structures/SumAndAverage/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace SumAndAverage
+{
+    using System;
+    using System.Linq;
+    using System.Collections.Generic;
+    public class Program
+    {
+        public static void Main()
+        {
+            var list = new List<int>();
+            list = Console.ReadLine().Split().Select(int.Parse).ToList();
+            var sum = list.Sum();
+            var average = list.Average();
+            Console.WriteLine($"Sum={sum}; Average={average:F2}");
+        }
+    }
+}
