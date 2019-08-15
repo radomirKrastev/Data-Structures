2f2f47a31e624684ef801a5a42faa57e99773e90
radomirKrastev <radomirkostantinov@gmail.com>
2019-08-15 16:37:42 +0300

    exercise

--- Stacks and Queues/CalculateSequenceWithQueueAnother/Program.cs
+++ Stacks and Queues/CalculateSequenceWithQueueAnother/Program.cs
@@ -1,12 +1,32 @@
-﻿using System;
-
-namespace CalculateSequenceWithQueueAnother
+﻿namespace CalculateSequenceWithQueueAnother
 {
-    class Program
+    using System;
+    using System.Collections.Generic;
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main()
         {
-            Console.WriteLine("Hello World!");
+            var number = int.Parse(Console.ReadLine());
+            var queue = new Queue<int>();
+            queue.Enqueue(number);
+            var arr = new int[50];
+            var elementCounter = 0;
+
+            while(queue.Count>0)
+            {
+                if (elementCounter == 50)
+                {
+                    break;
+                }
+                arr[elementCounter] = queue.Dequeue();
+
+                queue.Enqueue(arr[elementCounter] + 1);
+                queue.Enqueue(2*arr[elementCounter] + 1);
+                queue.Enqueue(arr[elementCounter] + 2);
+                elementCounter++;
+            }
+
+            Console.WriteLine(string.Join(", ",arr));
         }
     }
 }
