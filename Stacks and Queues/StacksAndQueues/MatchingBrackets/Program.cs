e2b1f7e882e09f7a05419e7ffe50cbf2e5964ad0
radomirKrastev <radomirkostantinov@gmail.com>
2019-08-15 10:33:03 +0300

    exercise

---
+++ Stacks and Queues/StacksAndQueues/MatchingBrackets/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace MatchingBrackets
+{
+    namespace MatchingBrackets
+    {
+        using System;
+        using System.Collections.Generic;
+        public class Program
+        {
+            public static void Main()
+            {
+                Stack<int> stack = new Stack<int>();
+                var example = "1 + (2 - (2 + 3) * 4 / (3 + 1)) * 5";
+
+                for (int i = 0; i < example.Length; i++)
+                {
+                    if (example[i] == '(')
+                    {
+                        stack.Push(i);
+                    }
+                    else if (example[i] == ')')
+                    {
+                        var startPoint = stack.Pop();
+                        var result = example.Substring(startPoint, i - startPoint + 1);
+                        Console.WriteLine(result);
+                    }
+                }
+            }
+        }
+    }
+}
