3999a86b0f203c6f1b92c18241c2b795b647f253
radomirKrastev <radomirkostantinov@gmail.com>
2019-08-15 11:25:34 +0300

    stack structure

---
+++ Stacks and Queues/StacksAndQueues/LinkedStack/Launcher.cs
@@ -0,0 +1,10 @@
+﻿namespace LinkedStack
+{
+    public class Launcher
+    {
+        public static void Main()
+        {
+            LinkedStack<int> stack = new LinkedStack<int>();
+        }
+    }
+}
---
+++ Stacks and Queues/StacksAndQueues/LinkedStack/Program.cs
@@ -0,0 +1,77 @@
+﻿namespace LinkedStack
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    public class LinkedStack<T> : IEnumerable<T>
+    {
+        public int Count { get; set; }
+        private StackNode top;
+
+        public void Push(T element)
+        {
+            this.top = new StackNode(element, this.top);
+            this.Count++;
+        }
+
+        public T Pop()
+        {
+            if (this.Count == 0)
+            {
+                throw new InvalidOperationException();
+            }
+
+            StackNode result = this.top;
+            this.top = this.top.Next;
+            this.Count--;
+            return result.Value;            
+        }
+
+        public T Peak()
+        {
+            if (this.Count == 0)
+            {
+                throw new InvalidOperationException();
+            }
+
+            return this.top.Value;
+        }
+
+        public T[] ToArray()
+        {
+            T[] array = new T[this.Count];
+            StackNode current = this.top;
+            int index = 0;
+
+            while (current != null)
+            {
+                array[index++] = current.Value;
+                current = current.Next;
+            }
+
+            return array;
+        }
+
+        IEnumerator<T> IEnumerable<T>.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        private class StackNode
+        {
+            public T Value { get; set; }
+            public StackNode Next { get; set; }
+
+            public StackNode (T value, StackNode next)
+            {
+                this.Value = value;
+                this.Next = next;
+            }
+        }
+    }
+}
