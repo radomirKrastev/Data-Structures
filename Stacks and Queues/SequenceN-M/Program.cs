2c82c1c75988009bec7ce432365b5de66846c072
radomirKrastev <radomirkostantinov@gmail.com>
2019-08-18 12:24:52 +0300

    exercise

---
+++ Stacks and Queues/SequenceN-M/Program.cs
@@ -0,0 +1,59 @@
+﻿namespace SequenceN_M
+{
+    using System;
+    using System.Linq;
+    using System.Collections.Generic;
+    public class Program
+    {
+        public static void Main()
+        {
+            var startToEnd = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            var start = startToEnd[0];
+            var end = startToEnd[1];
+
+            Queue<Item> items = new Queue<Item>();
+            items.Enqueue(new Item(start));
+
+            while (items.Count != 0)
+            {
+                var item = items.Dequeue();
+                if (item.Value < end)
+                {
+                    items.Enqueue(new Item(item.Value + 1) { Previous = item });
+                    items.Enqueue(new Item(item.Value + 2) { Previous = item });
+                    items.Enqueue(new Item(item.Value * 2) { Previous = item });
+                }
+
+                if (item.Value == end)
+                {
+                    PrintSolution(item);
+                    break;
+                }
+            }
+        }
+
+        public static void PrintSolution(Item item)
+        {
+            var shortestSequence = new List<int>();
+            while (item != null)
+            {
+                shortestSequence.Add(item.Value);
+                item = item.Previous;
+            }
+
+            shortestSequence.Reverse();
+            Console.WriteLine(string.Join(" -> ", shortestSequence));
+        }
+
+        public class Item
+        {
+            public int Value { get; set; }
+            public Item Previous { get; set; }
+
+            public Item(int value)
+            {
+                this.Value = value;
+            }
+        }
+    }
+}
