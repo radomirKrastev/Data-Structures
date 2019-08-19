7d8f8a7ca10ddec94c5ba9afa1463a02fb5ec6df
radomirKrastev <radomirkostantinov@gmail.com>
2019-08-19 13:57:45 +0300

    Implement Tree

---
+++ Basic Tree Data Structures - Tree, Binary Tree/Trees/Trees/Tree.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Tree<T>
+{
+    public T Value { get; set; }
+    public List<Tree<T>> Children { get; private set; }
+
+    public Tree(T value, params Tree<T>[] children)
+    {
+        this.Value = value;
+        this.Children = new List<Tree<T>>();
+        foreach (var child in children)
+        {
+            this.Children.Add(child);
+        }
+    }
+
+    public void Print(int indent = 0)
+    {
+        Console.Write(new string(' ', 2 * indent));
+        Console.WriteLine(this.Value);
+        foreach (var child in Children)
+        {
+            child.Print(indent + 1);
+        }
+    }
+
+    public void Each(Action<T> action)
+    {
+        action(this.Value);
+        foreach (var child in Children)
+        {
+            child.Each(action);
+        }
+    }
+
+    public IEnumerable<T> OrderDFS()
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<T> OrderBFS()
+    {
+        throw new NotImplementedException();
+    }
+}
